acb8cf06ee72ee018f36e880de234a046a268f69
SajedehAdelia <adelia.fathipour@gmail.com>
2024-05-21 16:58:53 +0200

    Improve the structure

--- BreadyToomy_Foody/BreadyToomy_Foody/Models/Product.cs
+++ BreadyToomy_Foody/BreadyToomy_Foody/Models/Product.cs
@@ -1,101 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
 namespace BreadyToomy_Foody.Models
 {
-    public class Product : INotifyPropertyChanged
+    public class Product
     {
-        private int _id;
-        public int Id
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProductType Type { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(int id, string name, ProductType type, decimal price)
         {
-            get => _id;
-            set
-            {
-                _id = value;
-                OnPropertyChanged();
-            }
+            Id = id;
+            Name = name;
+            Type = type;
+            Price = price;
         }
 
-        private string _name;
-        public string Name
+        public decimal calculeTotalPrice(int quantity)
         {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Recipe _recipe;
-        public Recipe Recipe
-        {
-            get => _recipe;
-            set
-            {
-                _recipe = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private ProductType _type;
-        public ProductType Type
-        {
-            get => _type;
-            set
-            {
-                _type = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _description;
-        public string Description
-        {
-            get => _description;
-            set
-            {
-                _description = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private decimal _price;
-        public decimal Price
-        {
-            get => _price;
-            set
-            {
-                _price = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private bool _archived;
-        public bool Archived
-        {
-            get => _archived;
-            set
-            {
-                _archived = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return quantity * Price;
         }
     }
 
     public enum ProductType
     {
-        Meal,
-        Drink,
-        Snack
+        Food,
+        Drinks,
+        Snacks
+
     }
 }
+
