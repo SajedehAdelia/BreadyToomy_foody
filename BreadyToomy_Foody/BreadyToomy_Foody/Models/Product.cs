e3495fdd808fa48fd0a7793f37b599d871770c6b
SajedehAdelia <adelia.fathipour@gmail.com>
2024-05-21 16:24:04 +0200

    Add product model and manager
    Still in progress

---
+++ BreadyToomy_Foody/BreadyToomy_Foody/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BreadyToomy_Foody.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public ProductType Type { get; set; }
+		public decimal Price { get; set; }
+
+		public Product(int id, string name, ProductType type, decimal price)
+		{
+			Id = id;
+			Name = name;
+			Type = type;
+			Price = price;
+		}
+
+		public decimal calculeTotalPrice(int quantity)
+		{
+			return quantity * Price;
+		}
+	}
+
+	public enum ProductType
+	{
+		Food,
+		Drinks,
+		Snacks
+
+	}
+}
+
