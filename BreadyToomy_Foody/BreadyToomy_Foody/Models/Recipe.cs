6b0e3a89e17ed743d6f7167fc63b823596e44a38
SajedehAdelia <adelia.fathipour@gmail.com>
2024-05-24 14:35:35 +0200

    Recipe Model

---
+++ BreadyToomy_Foody/BreadyToomy_Foody/Models/Recipe.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BreadyToomy_Foody.Models
+{
+    public class Recipe : INotifyPropertyChanged
+    {
+		private int _id;
+		private string _difficulty;
+		private int _cookTime;
+		private bool _archived;
+        private List<RecipeStep> _steps;
+
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Difficulty
+        {
+            get => _difficulty;
+            set
+            {
+                _difficulty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int CookTime
+        {
+            get => _cookTime;
+            set
+            {
+                _cookTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public bool Archived
+        {
+            get => _archived;
+            set
+            {
+                _archived = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<RecipeStep> Steps
+        {
+            get => _steps;
+            set
+            {
+                _steps = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ BreadyToomy_Foody/BreadyToomy_Foody/ViewModels/RecipeManagementViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using BreadyToomy_Foody.Models;
+
+namespace BreadyToomy_Foody.ViewModels
+{
+	public class RecipeManagementViewModel : INotifyPropertyChanged
+    {
+		private ObservableCollection<Recipe> _recipes;
+		private Recipe _selectedRecipe;
+
+        public ObservableCollection<Recipe> Recipes
+		{
+			get => _recipes;
+			set
+			{
+				_recipes = value;
+				OnPropertyChanged();
+			}
+		}
+
+        public Recipe SelectedRecipe
+        {
+            get => _selectedRecipe;
+            set
+            {
+                _selectedRecipe = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            Recipes.Add(recipe);
+        }
+
+        public void UpdateRecipe(Recipe recipe)
+        {
+            var existingRecipe = Recipes.FirstOrDefault(r => r.Id == recipe.Id);
+            if (existingRecipe != null)
+            {
+                existingRecipe.Difficulty = recipe.Difficulty;
+                existingRecipe.CookTime = recipe.CookTime;
+                existingRecipe.Archived = recipe.Archived;
+                OnPropertyChanged(nameof(Recipes));
+            }
+        }
+
+        public void DeleteRecipe(Recipe recipe)
+        {
+            Recipes.Remove(recipe);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public RecipeManagementViewModel()
+		{
+            Recipes = new ObservableCollection<Recipe>
+            {
+                //DATA
+            };
+		}
+	}
+}
+
